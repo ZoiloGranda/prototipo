c0833244fdb7153d75d48a60b7893299532ec1e9
Zoilo Granda <zgranda@gmail.com>
2017-06-28 10:33:21 -0400

    añadido puerto para conexion con heroku

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var favicon = require('serve-favicon');
 var _ = require('lodash');
 
 var app = express();
+app.set('port', (process.env.PORT || 4000));
+
 
 var connection = mysql.createPool({
   connectionLimit: 20,
@@ -110,4 +112,6 @@ app.post('/relatorio', function(req, res) {
   })
 })
 
-app.listen(4000);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
